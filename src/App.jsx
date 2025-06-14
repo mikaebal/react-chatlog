f242789f19b5519581f406f4dfda0098130284c2
Mikaela Baluyot <mikaebal@gmail.com>
2025-06-14 16:38:39 -0700

    feat: implement Wave 1 with ChatEntry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
 
 const App = () => {
+  const chatMessages = messages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatMessages.sender}
+          body={chatMessages.body}
+          timeStamp={chatMessages.timeStamp}
+        />
       </main>
     </div>
   );
